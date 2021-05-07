00ecfe4733965dbb154e546b47a0a0ac84588532
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-05-07 11:38:10 +0200

    Connect with redux our app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import styled, { createGlobalStyle } from "styled-components";
+import List from "./components/List/List";
+import { fetchCharacters } from "./data/actions/characters.actions";
 
 function App() {
+  const { characters } = useSelector((state) => state.characters);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, []);
+
   return (
     <div className="App">
       <GlobalStyle />
       <StyledHeader>Star Wars App</StyledHeader>
+      <List />
     </div>
   );
 }
@@ -19,6 +30,7 @@ const StyledHeader = styled.h1`
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
 *{
   font-family:"Roboto",sans-serif;
   margin:0;
