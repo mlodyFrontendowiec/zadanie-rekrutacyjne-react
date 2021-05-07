828e33b2159e7a3ac186405ef881997a74f353af
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-05-07 10:02:01 +0200

    Add simple elements to main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <StyledHeader>Star Wars App</StyledHeader>
+    </div>
+  );
 }
+
+const StyledHeader = styled.h1`
+  font-size: 25px;
+  color: black;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+*{
+  font-family:"Roboto",sans-serif;
+  margin:0;
+  padding:0;
+}
+`;
 
 export default App;
